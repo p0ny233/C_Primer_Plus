e87836ccef165b968ea72dd3d75c16775bb3bbdf
p0ny233 <p0ny233@github.com>
2023-04-14 01:58:18 -0700

    test inline function

---
+++ randomEXP/12_test_inline_function_define.c
@@ -0,0 +1,21 @@
+// no finished
+#include <stdio.h>
+#define FORCE_FUNCTION __attribute__((always_inline))
+
+//inline static int demo1(int a, int b);
+FORCE_FUNCTION inline static int demo1(int a, int b);
+
+
+int main(void)
+{
+
+    printf("result = %d\n", demo1(1, 2));
+
+    return 0;
+}
+
+int demo1(int a, int b)
+{
+
+    return a < b ? a : b;
+}
